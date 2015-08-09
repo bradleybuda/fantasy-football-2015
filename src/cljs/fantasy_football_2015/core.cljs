341f8082725f9d22e00b9171fb3c44738fc18240
Bradley Buda <bradleybuda@gmail.com>
2015-08-09 13:46:59 -0700

    Show available players

--- src/cljs/fantasy_football_2015/core.cljs
+++ src/cljs/fantasy_football_2015/core.cljs
@@ -1,18 +1,53 @@
 (ns fantasy-football-2015.core
     (:require [reagent.core :as reagent]))
 
-(defonce app-state (reagent/atom {:text "Hello, what is your name? "}))
+;; TODO should I be division-aware? Might be useful to penalize my
+;; division opponents more
+
 
 (def roster-composition
   [:qb :rb :rb :wr :wr :te :flex :d/st :k
    :bench :bench :bench :bench :bench :bench :bench])
 
-(defn page []
+;; TODO update with proper order
+(def members-in-draft-order
+  [:larry-whalen :david-baum :blake-wilson :michael-aslanides
+   :kevin-worley :will-morton :bradley-buda :sam-strasfeld
+   :jeff-mazer :eric-eleton :nick-horton :josh-harris])
+
+(def all-players
+  [:alice :bob :charlie])
+
+(defonce app-state
+  (reagent/atom
+   {:available-players all-players
+    :picked-players []}))
+
+(def me :bradley-buda)
+
+(def members-table
   [:table
-   [:tr
-    (for [[idx roster-position] (map-indexed (fn [idx item] [idx (str item)]) roster-composition)]
-      ^{:key idx} [:td roster-position])
-    ]])
+   [:th "Draft Selections"]
+   (for [[member-idx member] (map-indexed (fn [member-idx member] [member-idx (str member)]) members-in-draft-order)]
+     ^{:key member-idx}
+     [:tr
+      [:th member]
+      (for [[idx roster-position] (map-indexed (fn [idx item] [idx (str item)]) roster-composition)]
+        ^{:key idx} [:td roster-position])])])
+
+(def players-table
+  [:table
+   [:th "Available Players"]
+   (for [[player-idx player] (map-indexed (fn [player-idx player] [player-idx (str player)]) all-players)]
+     ^{:key player-idx}
+     [:tr
+      [:td player]])])
+
+(defn page []
+  [:div
+   members-table
+   players-table]
+  )
 
 (defn ^:export main []
   (reagent/render [page]
