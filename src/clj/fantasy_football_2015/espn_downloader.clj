ae051d66edb816d42c02c3bd4d8090048ef26bca
Bradley Buda <bradleybuda@gmail.com>
2015-08-16 14:27:42 -0700

    Write / generate players to source file

--- src/clj/fantasy_football_2015/espn_downloader.clj
+++ src/clj/fantasy_football_2015/espn_downloader.clj
@@ -1,14 +1,16 @@
 (ns fantasy-football-2015.espn-downloader
-  (:require [net.cgrand.enlive-html :as html]))
+  (:require [net.cgrand.enlive-html :as html]
+            [clojure.java.io :as io]))
 
 (def page-to-scrape "http://espn.go.com/fantasy/football/story/_/id/12866396/top-300-rankings-2015")
+(def output-file "src/cljs/fantasy_football_2015/generated/espn.cljs")
 
 (defn parse-table-row [row]
   (let [cells (html/select row [:td])
-        [rank-player-position team bye posrank value] (map html/text cells)
-        [_ rank player position] (re-matches #"^(\d+)\. ([^,]+), (.*)$" rank-player-position)]
+        [rank-name-position team bye posrank value] (map html/text cells)
+        [_ rank name position] (re-matches #"^(\d+)\. ([^,]+), (.*)$" rank-name-position)]
     {:rank (read-string rank)
-     :player player
+     :name name
      :position position
      :team team
      :value (if (= "--" value) 0 (read-string (clojure.string/replace value "$" "")))}))
@@ -16,5 +18,9 @@
 (defn -main []
   (let [resource (html/html-resource (java.net.URL. page-to-scrape))
         table (second (html/select resource [:table.inline-table]))
-        table-rows (html/select table [:tbody :tr])]
-    (pr (map parse-table-row table-rows))))
+        table-rows (html/select table [:tbody :tr])
+        players (map parse-table-row table-rows)]
+
+    (with-open [wrtr (io/writer output-file)]
+      (.write wrtr (prn-str '(ns fantasy-football-2015.generated.espn)))
+      (.write wrtr (prn-str `(def ~'all-players ~players))))))
