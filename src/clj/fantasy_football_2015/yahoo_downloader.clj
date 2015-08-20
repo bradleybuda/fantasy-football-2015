c78503b8b91dbf9578c80e1a3c2200768a106569
Bradley Buda <bradleybuda@gmail.com>
2015-08-20 13:22:50 -0400

    Add other fields to player

--- src/clj/fantasy_football_2015/yahoo_downloader.clj
+++ src/clj/fantasy_football_2015/yahoo_downloader.clj
@@ -5,18 +5,20 @@
 (def url-template "https://football.fantasysports.yahoo.com/f1/draftanalysis?tab=AD&pos=ALL&sort=DA_AP&count=")
 
 (defn parse-table-row [row]
-  (let [name-team-position-cell (html/select row [:div.ysf-player-name])
+  (let [[name-team-position-cell _ avg-cost-cell _] (html/select row [:td])
         name (html/text (first (html/select name-team-position-cell [:.name])))
         team-position (first (html/select name-team-position-cell [:.Fz-xxs]))
-        [_ team position] (re-matches #"^([^ ]{2,3}) - ([^ ]+)$" (html/text team-position))]
+        [_ team position] (re-matches #"^([^ ]{2,3}) - ([^ ]+)$" (html/text team-position))
+        value (read-string (clojure.string/replace (html/text avg-cost-cell) "$" ""))]
     {:name name
      :position position
-     :team team}))
+     :team team
+     :value value}))
 
 (defn -main []
   (let [url (str url-template "0") ;; TODO fetch additional pages
         resource (html/html-resource (java.net.URL. url-template))
         table (html/select resource [:table#draftanalysistable])
         table-rows (html/select table [:tbody :tr])]
-    (prn (first table-rows))
+    (prn (html/select  (first table-rows) [:td]))
     (prn (map parse-table-row table-rows))))
